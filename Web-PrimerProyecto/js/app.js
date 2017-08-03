7f0ec2e5a42f39dc1d018fbfd6476f0787911cc3
MaycoGG <maycogarelli@gmail.com>
2017-08-03 19:16:04 -0300

    navbar-Avanzado

---
+++ Web-PrimerProyecto/js/app.js
@@ -0,0 +1,52 @@
+﻿var app = angular.module("app", ['ngRoute']);
+
+app.controller("PrincipalController", function ($scope) {
+    
+})
+
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            template: ''
+        })
+        .when('/empresa', {
+            templateUrl: 'Empresa.html',
+            controller: 'EmpresaController'
+        })
+        .when('/productos', {
+            templateUrl: 'Productos.html',
+            controller: 'ProductosController'
+        })
+        .when('/promociones', {
+            templateUrl: 'Promociones.html',
+            controller: 'PromocionesController'
+        })
+        .when('/contacto', {
+            templateUrl: 'Contacto.html',
+            controller: 'ContactoController'
+        })
+        .when('/pnf', {
+            templateUrl: 'pnf.html'
+        })
+        .otherwise({
+            redirecTo: '/'
+        })
+}])
+
+
+app.controller("EmpresaController", function ($scope) {
+    $scope.descripcion = "Empresa"
+})
+
+app.controller("PromocionesController", function ($scope) {
+    $scope.descripcion = "Promociones"
+})
+
+app.controller("ProductosController", function ($scope) {
+    $scope.descripcion = "Productos"
+})
+
+app.controller("ContactoController", function ($scope) {
+    $scope.descripcion = "Contacto"
+})
